364f213f7ac9771ff93d12316911ee80ff259920
danica-nami <53499847+danica-nami@users.noreply.github.com>
2019-08-12 22:50:26 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+ <title>My Profile</title>
+</head>
+<body>
+ <h1 style="text-align:center;">GimedaD Photo Gallery</h1>
+<br>
+<br>
+<br>
+<h3 style="text-align:center;">List of Photos</h3>
+<br>
+<br>
+
+<nav style="text-align:center;">
+ <a href="photo1.html">Photo 1</a><br>
+ <a href="photo2.html">Photo 2</a><br>
+ <a href="photo3.html">Photo 3</a>
+ 
+</nav>
+
+
+
+</body>
+</html>
